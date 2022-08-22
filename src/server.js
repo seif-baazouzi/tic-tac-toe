0a2e6e98ba6606c222ca5887c9dcccaf0077e4c3
seif <baazouziseif9@gmail.com>
2022-08-22 14:37:04 +0100

    check player win

--- src/server.js
+++ src/server.js
@@ -55,6 +55,7 @@ server.connection((ws) => {
       })
 
       server.on(room.paler1, "click", (field) => {
+        if(room.endGame) return
         if(room.currentPlayer != "p1") return
         if(room.fields[field-1] != field) return
 
@@ -63,9 +64,16 @@ server.connection((ws) => {
         server.emit(room.paler2, "set-field", { field, content: "X", message: "Player 2" })
 
         room.currentPlayer = "p2"
+
+        if(checkWin(room.fields, "X")) {
+          room.endGame = true
+          server.emit(room.paler1, "message", "Player 1 WIN")
+          server.emit(room.paler2, "message", "Player 1 WIN")
+        }
       })
 
       server.on(room.paler2, "click", (field) => {
+        if(room.endGame) return
         if(room.currentPlayer != "p2") return
         if(room.fields[field-1] != field) return
         
@@ -74,11 +82,18 @@ server.connection((ws) => {
         server.emit(room.paler2, "set-field", { field, content: "O", message: "Player 1" })
 
         room.currentPlayer = "p1"
+
+        if(checkWin(room.fields, "O")) {
+          room.endGame = true
+          server.emit(room.paler1, "message", "Player 2 WIN")
+          server.emit(room.paler2, "message", "Player 2 WIN")
+        }
       })
     } else {
       const room = {
         paler1: ws,
         paler2: null,
+        endGame: false,
         currentPlayer: "p1",
         fields: [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
       }
@@ -88,3 +103,18 @@ server.connection((ws) => {
     }
   })
 })
+
+function checkWin(fields, symbol) {
+  return (
+    (fields[0] === fields[1] && fields[1] === fields[2] && fields[2] === symbol) ||
+    (fields[3] === fields[4] && fields[4] === fields[5] && fields[5] === symbol) ||
+    (fields[6] === fields[7] && fields[7] === fields[8] && fields[8] === symbol) ||
+
+    (fields[0] === fields[3] && fields[3] === fields[6] && fields[6] === symbol) ||
+    (fields[1] === fields[4] && fields[4] === fields[7] && fields[7] === symbol) ||
+    (fields[2] === fields[5] && fields[5] === fields[8] && fields[8] === symbol) ||
+
+    (fields[0] === fields[4] && fields[4] === fields[8] && fields[8] === symbol) ||
+    (fields[2] === fields[4] && fields[4] === fields[6] && fields[6] === symbol)
+  )
+}
